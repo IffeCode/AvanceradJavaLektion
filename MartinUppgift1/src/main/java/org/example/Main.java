db9a0030f90c05440c298bd745fa0a7fabd42fde
iffe <iffe.h@hotmail.com>
2025-12-02 20:59:57 +0100

    Gjort första delen av uppgift 2!

--- MartinUppgift1/src/main/java/org/example/Main.java
+++ MartinUppgift1/src/main/java/org/example/Main.java
@@ -34,6 +34,30 @@ public class Main {
         double cylinderVolume = pi * Math.pow(radius, 2) * height;
         System.out.println("The cylinder volume is: " + cylinderVolume);
 
+        //Skriv rätt datatyp:
+
+        int myVar1 = 99;
+        double myVar2 = 3.14f;
+        char myVar3 = 'A';
+        boolean myVar4 = true;
+        String myVar5 = "Hello, World!";
+
+        double y = (double) 1 /3; //måste sätta double inom parantes framför.
+        System.out.println("1/3 = " + y);
+
+        double z = 0.1 +0.1 +0.1 +0.1 +0.1 +0.1 +0.1 +0.1 +0.1 +0.1; //?
+        System.out.println("10 * 0,1 = " + z);
+
+        //Konvertera:
+
+        String string1 = "2.23";// till float
+        String.valueOf(string1);
+        String string2 = "false";// till boolean
+        String.valueOf(string2);
+        String string3 = "1337"; // till integer
+        int stringInteger = Integer.parseInt(string3);
+        System.out.println(string1 + " " + string2 + " " + " " + stringInteger);
+
 
 
 
