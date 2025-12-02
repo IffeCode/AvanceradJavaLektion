26ac04b484ef0b82cf10b89b9c23767e63c464fe
iffe <iffe.h@hotmail.com>
2025-12-02 20:20:42 +0100

    Påbörjat uppgift 2!

---
+++ MartinUppgift1/src/main/java/org/example/uppgift2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class uppgift2 {
+    //Skriv rätt datatyp:
+
+   int myVar1 = 99;
+   double myVar2 = 3.14f;
+   char myVar3 = 'A';
+   boolean myVar4 = true;
+   String myVar5 = "Hello, World!";
+}
